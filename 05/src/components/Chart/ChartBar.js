66e64318baac5c0d301d2c55daee44be4a52c7bf
Kiran Bansode <kiran5120135@gmail.com>
2022-04-08 16:26:54 +0530

    05.09. Wrap Up & Next Steps

--- 05/src/components/Chart/ChartBar.js
+++ 05/src/components/Chart/ChartBar.js
@@ -4,13 +4,13 @@ import "./ChartBar.css";
 const ChartBar = (props) => {
 	let barFillHeight = "0%";
 
-	if (props.max > 0) {
+	if (props.maxValue > 0) {
 		barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
 	}
 
 	return (
 		<div className="chart-bar">
-			<div className="char-bar__inner">
+			<div className="chart-bar__inner">
 				<div
 					className="chart-bar__fill"
 					style={{ height: barFillHeight }}
