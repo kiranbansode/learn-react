fb351ba56b603d5f667922aca4140177693bbb38
Kiran Bansode <kiran5120135@gmail.com>
2022-04-19 15:28:47 +0530

    12.05. Preventing Unnecessary Re-Evaluations with React.memo()

--- 12/src/components/UI/Button/Demo/DemoOutput.js
+++ 12/src/components/UI/Button/Demo/DemoOutput.js
@@ -3,8 +3,7 @@ import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
 	console.log("DemoOutput Running");
-
 	return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
