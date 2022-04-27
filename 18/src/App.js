b9f40575ce5400bbfb81dbbedb5b10d42e02fbd1
Kiran Bansode <kiran5120135@gmail.com>
2022-04-27 16:59:23 +0530

    18.22. Reading & Dispatching From A New Slice

--- 18/src/App.js
+++ 18/src/App.js
@@ -1,12 +1,15 @@
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+	const { isAuthenticated } = useSelector(({ auth }) => auth);
 	return (
 		<>
 			<Header />
-			<Auth />
+			{!isAuthenticated ? <Auth /> : <UserProfile />}
 			<Counter />
 		</>
 	);
