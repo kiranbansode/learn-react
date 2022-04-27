d6248b5d33f63eb553afa72c1d5908754a154895
Kiran Bansode <kiran5120135@gmail.com>
2022-04-27 15:33:21 +0530

    18.14. Attaching Payloads to Actions

--- 18/src/store/index.js
+++ 18/src/store/index.js
@@ -3,6 +3,9 @@ import { createStore } from "redux";
 const counterReducer = (state = { counter: 0 }, action) => {
 	if (action.type === "increment") return { counter: state.counter + 1 };
 
+	if (action.type === "increase")
+		return { counter: state.counter + action.amount };
+
 	if (action.type === "decrement") return { counter: state.counter - 1 };
 
 	return state;
