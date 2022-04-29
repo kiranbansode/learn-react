8435c615ef7ffb870f17cf20e0a524cb8bc6a000
Kiran Bansode <kiran5120135@gmail.com>
2022-04-29 19:34:07 +0530

    20.14. Understanding Relative & Absolute Paths

--- 20/src/pages/ProductDetail.js
+++ 20/src/pages/ProductDetail.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ProductDetailPage() {
 	const param = useParams();
@@ -7,6 +8,11 @@ function ProductDetailPage() {
 		<>
 			<h1>Product Detail</h1>
 			<p>{param.productId}</p>
+			<p>
+				<Link to=".." relative="path">
+					Back
+				</Link>
+			</p>
 		</>
 	);
 }
--- 20/src/pages/Products.js
+++ 20/src/pages/Products.js
@@ -13,7 +13,9 @@ function Products() {
 			<ul>
 				{PRODUCTS.map((prod) => (
 					<li key={prod.id}>
-						<Link to={`/products/${prod.id}`}>{prod.title}</Link>
+						<Link to={prod.id} relative="">
+							{prod.title}
+						</Link>
 					</li>
 				))}
 			</ul>
