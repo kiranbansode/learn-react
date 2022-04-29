66a88d5ee4994db5acb2a19e0b4a3ee240db89af
Kiran Bansode <kiran5120135@gmail.com>
2022-04-29 17:01:11 +0530

    20.08. Layouts & Nested Routes

--- 20/src/App.js
+++ 20/src/App.js
@@ -1,11 +1,12 @@
 import {
 	createBrowserRouter,
 	RouterProvider,
-	createRoutesFromElements,
-	Route,
+	// createRoutesFromElements,
+	// Route,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 // const routeDefinitions = createRoutesFromElements(
 // 	<Route>
@@ -16,8 +17,14 @@ import Products from "./pages/Products";
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-	{ path: "/", element: <HomePage /> },
-	{ path: "/products", element: <Products /> },
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ path: "/", element: <HomePage /> },
+			{ path: "/products", element: <Products /> },
+		],
+	},
 ]);
 
 function App() {
