3a86fe78011a4d2952f63fa53a3ec783a806dd60
Kiran Bansode <kiran5120135@gmail.com>
2022-04-30 12:05:01 +0530

    20.33. Working with action() Functions

--- 20/frontend/src/App.js
+++ 20/frontend/src/App.js
@@ -26,7 +26,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailsPage, {
 	loader as eventsDetailsLoader,
 } from "./pages/EventDetails";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -62,7 +62,7 @@ const router = createBrowserRouter([
 						],
 					},
 
-					{ path: "new", element: <NewEventPage /> },
+					{ path: "new", element: <NewEventPage />, action: newEventAction },
 				],
 			},
 		],
--- 20/frontend/src/pages/NewEvent.js
+++ 20/frontend/src/pages/NewEvent.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
@@ -5,3 +6,28 @@ function NewEventPage() {
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+	const data = await request.formData();
+
+	const eventData = {
+		title: data.get("title"),
+		date: data.get("date"),
+		description: data.get("description"),
+		image: data.get("image"),
+	};
+
+	const response = await fetch("http://localhost:8080/events", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(eventData),
+	});
+
+	if (!response.ok) {
+		throw json({ message: "Could not save event" }, { status: 500 });
+	}
+
+	return redirect("/events");
+}
