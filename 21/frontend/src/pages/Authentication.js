61583b7f48dc5247217cabba7f1676942847a17b
Kiran Bansode <kiran5120135@gmail.com>
2022-05-02 14:14:25 +0530

    21.08. Attaching Auth Tokens to Outgoing Requests

--- 21/frontend/src/pages/Authentication.js
+++ 21/frontend/src/pages/Authentication.js
@@ -42,6 +42,10 @@ export async function action({ request, params }) {
 		);
 	}
 
-	// soon manage token
+	const resData = await response.json();
+	const token = resData.token;
+
+	localStorage.setItem("token", token);
+
 	return redirect("/");
 }
