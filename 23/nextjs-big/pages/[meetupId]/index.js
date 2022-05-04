d0f08a8a98a5c293abc66f58a522b9f8c020c68c
Kiran Bansode <kiran5120135@gmail.com>
2022-05-04 17:34:05 +0530

    23.24. Working with Params for SSG Data Fetching

--- 23/nextjs-big/pages/[meetupId]/index.js
+++ 23/nextjs-big/pages/[meetupId]/index.js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
@@ -9,6 +10,26 @@ function MeetupDetails() {
 			description="The meetup Description"
 		/>
 	);
+}
+
+export async function getStaticProps(context) {
+	// fetch data for a single meetup
+
+	const meetupId = context.params.meetupId;
+
+	console.log(meetupId);
+
+	return {
+		props: {
+			meetupData: {
+				image:
+					"https://upload.wikimedia.org/wikipedia/commons/,thumb/2/21/Sunset_gondola_Basilica_Della_Salute_.png/1280px-Sunset_gondola_Basilica_Della_Salute_.png",
+				title: "A First Meetup",
+				address: "Some Street 5, Some City",
+				description: "The meetup Description",
+			},
+		},
+	};
 }
 
 export default MeetupDetails;
