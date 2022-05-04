7a7883a92882859ae8bab31d45aa01469c9bd314
Kiran Bansode <kiran5120135@gmail.com>
2022-05-04 14:35:50 +0530

    23.11. Extracting Dynamic Parameter Values

--- 23/pages/news/[newsId].js
+++ 23/pages/news/[newsId].js
@@ -1,5 +1,10 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/[dynamic-parameters]
 function DetailPage() {
+	const router = useRouter();
+	const newsId = router.query.newsId;
+
 	return <h1>The Detail Page</h1>;
 }
 
