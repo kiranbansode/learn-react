060b0b33983d42cedc90409bf635aee43fa6c4d9
Kiran Bansode <kiran5120135@gmail.com>
2022-05-04 18:06:05 +0530

    23.27. Working with MongoDB

--- 23/nextjs-big/api/new-meetup.js
+++ 23/nextjs-big/api/new-meetup.js
@@ -1,10 +1,25 @@
+import { MongoClient } from "mongodb";
+
 // api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
 	if (req.method === "POST") {
 		const data = req.body;
 		const { title, image, address, description } = data;
+
+		const client = await MongoClient.connect(
+			"mongodb+srv://kiran5120135:bJEvruNnMkZdz0hh@cluster0.of5j2ah.mongodb.net/?retryWrites=true&w=majority"
+		);
+		const db = client.db();
+
+		const meetupsCOllection = db.collection("meetups");
+		const result = await meetupsCOllection.insertOne(data);
+
+		console.log(result);
+
+		client.close();
+		res.status(201).json({ message: "Meetup inserted" });
 	}
 }
 
