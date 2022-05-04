03876b894022c2c4b0b515f16b493ed529bfd60b
Kiran Bansode <kiran5120135@gmail.com>
2022-05-04 16:42:35 +0530

    23.19. Adding Custom Components & CSS Modules

---
+++ 23/nextjs-big/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Sunset_gondola_Basilica_Della_Salute_.png/1280px-Sunset_gondola_Basilica_Della_Salute_.png"
+			title="A First Meetup"
+			address="Some Street 5, Some City"
+			description="The meetup Description"
+		/>
+	);
+}
+
+export default MeetupDetails;
