1fbf3fb9c0b92b2e4f68640c73bcf8ab4c40ea06
Kiran Bansode <kiran5120135@gmail.com>
2022-05-04 17:42:21 +0530

    23.25. Preparing Paths with getStaticPaths & Working With Fallback Pages

--- 23/nextjs-big/pages/[meetupId]/index.js
+++ 23/nextjs-big/pages/[meetupId]/index.js
@@ -12,6 +12,24 @@ function MeetupDetails() {
 	);
 }
 
+export async function getStaticPaths() {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: "m1",
+				},
+			},
+			{
+				params: {
+					meetupId: "m2",
+				},
+			},
+		],
+	};
+}
+
 export async function getStaticProps(context) {
 	// fetch data for a single meetup
 
@@ -24,6 +42,7 @@ export async function getStaticProps(context) {
 			meetupData: {
 				image:
 					"https://upload.wikimedia.org/wikipedia/commons/,thumb/2/21/Sunset_gondola_Basilica_Della_Salute_.png/1280px-Sunset_gondola_Basilica_Della_Salute_.png",
+				id: meetupId,
 				title: "A First Meetup",
 				address: "Some Street 5, Some City",
 				description: "The meetup Description",
