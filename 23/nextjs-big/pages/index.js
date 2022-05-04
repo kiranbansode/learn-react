ec60d15edcc842cf5a436562431831cdf2f57ce0
Kiran Bansode <kiran5120135@gmail.com>
2022-05-04 18:38:00 +0530

    23.29. Getting Data From The Database

--- 23/nextjs-big/pages/index.js
+++ 23/nextjs-big/pages/index.js
@@ -1,34 +1,5 @@
-import { useEffect, useState } from "react";
-import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-	{
-		id: "m1",
-		title: "A First Meetup",
-		image:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Sunset_gondola_Basilica_Della_Salute_.png/1280px-Sunset_gondola_Basilica_Della_Salute_.png",
-		address: "Some address 5,12345, Some City",
-		description: "This is a first meetup!",
-	},
-
-	{
-		id: "m2",
-		title: "A Second Meetup",
-		image:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Sunset_gondola_Basilica_Della_Salute_.png/1280px-Sunset_gondola_Basilica_Della_Salute_.png",
-		address: "Some address 5,12345, Some City",
-		description: "This is a Second meetup!",
-	},
-
-	{
-		id: "m3",
-		title: "A third Meetup",
-		image:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Sunset_gondola_Basilica_Della_Salute_.png/1280px-Sunset_gondola_Basilica_Della_Salute_.png",
-		address: "Some address 5,12345, Some City",
-		description: "This is a third meetup!",
-	},
-];
+import { MongoClient } from 'mongodb';
+import MeetupList from '../components/meetups/MeetupList';
 
 function HomePage(props) {
 	return <MeetupList meetups={props.meetups} />;
@@ -48,9 +19,22 @@ function HomePage(props) {
 
 export async function getStaticProps() {
 	// fetch data from API
+	const client = await MongoClient.connect(
+		'mongodb+srv://kiran5120135:bJEvruNnMkZdz0hh@cluster0.of5j2ah.mongodb.net/?retryWrites=true&w=majority'
+	);
+	const db = client.db();
+	const meetupsCOllection = db.collection('meetups');
+
+	const meetups = await meetupsCOllection.find().toArray();
+
+	client.close();
+
 	return {
 		props: {
-			meetups: DUMMY_MEETUPS,
+			meetups: meetups.map(({ _id, ...otherData }) => ({
+				id: _id.toString(),
+				...otherData,
+			})),
 		},
 		revalidate: 1,
 	};
