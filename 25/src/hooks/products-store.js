1c7a0105fbf7c9f163d6a7d8e24ebe8ec8f14761
Kiran Bansode <kiran5120135@gmail.com>
2022-05-06 15:28:05 +0530

    25.10. Using the Custom Store

--- 25/src/hooks/products-store.js
+++ 25/src/hooks/products-store.js
@@ -3,16 +3,16 @@ import { initStore } from './store';
 const configureStore = () => {
 	const actions = {
 		TOGGLE_FAV: (curState, productId) => {
-			const prodIndex = curState.findIndex((p) => p.id === productId);
-			const newFavStatus = !curState[prodIndex].isFavorite;
-			const updatedProducts = [...curState];
+			const prodIndex = curState.products.findIndex((p) => p.id === productId);
+			const newFavStatus = !curState.products[prodIndex].isFavorite;
+			const updatedProducts = [...curState.products];
 			updatedProducts[prodIndex] = {
-				...curState[prodIndex],
+				...curState.products[prodIndex],
 				isFavorite: newFavStatus,
 			};
 
 			return {
-				products: curState.updatedProducts,
+				products: curState.products.updatedProducts,
 			};
 		},
 	};
--- 25/src/index.js
+++ 25/src/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import ProductsProvider from './context/products-context';
+import configureProductStore from './hooks/products-store';
+
+configureProductStore();
 
 ReactDOM.render(
-	<ProductsProvider>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</ProductsProvider>,
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 	document.getElementById('root')
 );
