61591e9a0e5222bf0a23781ca4d967a20f47f2d0
Kiran Bansode <kiran5120135@gmail.com>
2022-05-10 14:36:02 +0530

    27.04. Exploring the Base Types

---
+++ 27/basics.ts
@@ -0,0 +1,12 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Functions types, parameters
+
+let age: number;
+age = 12;
+
+let username: string;
+username: 'kiran';
+
+let isInstructor: boolean;
+isInstructor = true;
