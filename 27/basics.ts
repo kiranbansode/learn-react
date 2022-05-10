2abf934a0564c6c4feb99b52b3617c3e272183f9
Kiran Bansode <kiran5120135@gmail.com>
2022-05-10 14:44:09 +0530

    27.05. Working with Array & Object Types

--- 27/basics.ts
+++ 27/basics.ts
@@ -1,12 +1,28 @@
-// Primitives: number, string, boolean
-// More complex types: arrays, objects
-// Functions types, parameters
+/* ------------------- Primitives: number, string, boolean ------------------ */
 
 let age: number;
 age = 12;
 
 let username: string;
-username: 'kiran';
+username = 'kiran';
 
 let isInstructor: boolean;
 isInstructor = true;
+
+/* ------------------- More complex types: arrays, objects ------------------ */
+let hobbies: string[];
+hobbies = ['Sports', 'Cooking'];
+
+let person: { name: string; age: number };
+person = {
+	name: 'kiran',
+	age: 32,
+};
+
+// person = {
+// 	isEmployee: false
+// };
+
+let people: { name: string; age: number }[];
+
+// Functions types, parameters
